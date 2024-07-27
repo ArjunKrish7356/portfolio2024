6a121f8ec62596b512ff761fbb94bea5fdf5b101
M Arjun Krishna <119443675+ArjunKrish7356@users.noreply.github.com>
2024-07-27 22:08:41 +0530

    travel app demo and github linked has been updated

--- src/app/Assets/Projects.js
+++ src/app/Assets/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import project2 from '../../../public/project2.png'
 import project1 from '../../../public/project1.png'
 
 function Projects() {
@@ -11,16 +12,16 @@ function Projects() {
         </div>
         <div className='project-container w-full h-fit grid grid-cols-1 gap-3 sm:gap-5 sm:grid-cols-2 lg:grid-cols-3 text-black p-5 place-items-center'>
            <div className='h-fit sm:w-fit  grid gap-3 lg:gap-4 xl:gap-6 p-5 border border-black rounded-3xl overflow-hidden justify-items-center'>
-              <Image className=' rounded-3xl justify-self-center sm:size-[200px] lg:size-[300px] xl:size-[400px]' src={project1}></Image>
-              <h1 className=' w-fit  text-xl font-semibold sm:text-sm lg:text-lg xl:text-2xl'>Project One</h1>
+              <Image className=' rounded-3xl justify-self-center sm:size-[200px] lg:size-[300px] xl:size-[400px]' src={project2}></Image>
+              <h1 className=' w-fit  text-xl font-semibold sm:text-sm lg:text-lg xl:text-2xl'>Travel App</h1>
               <div className=' w-fit h-fit flex justify-evenly gap-2 lg:gap-4'>
-                <button className=' btn btn-md sm:max-lg:btn-sm xl:btn-lg btn-outline hover:bg-black hover:text-white text-black rounded-full text-base sm:text-sm xl:text-xl'>Github</button>
-                <button className=' btn btn-md sm:max-lg:btn-sm xl:btn-lg  btn-outline hover:bg-black hover:text-white text-black rounded-full text-base sm:text-sm xl:text-xl'>Live Demo</button>
+                <a href='https://github.com/ArjunKrish7356/travelapp' className=' btn btn-md sm:max-lg:btn-sm xl:btn-lg btn-outline hover:bg-black hover:text-white text-black rounded-full text-base sm:text-sm xl:text-xl'>Github</a>
+                <a href='https://travelapp-ivory.vercel.app/' className=' btn btn-md sm:max-lg:btn-sm xl:btn-lg  btn-outline hover:bg-black hover:text-white text-black rounded-full text-base sm:text-sm xl:text-xl'>Live Demo</a>
               </div>
             </div>
             <div className='h-fit sm:w-fit  grid gap-3 lg:gap-4 xl:gap-6 p-5 border border-black rounded-3xl overflow-hidden justify-items-center'>
               <Image className=' rounded-3xl justify-self-center sm:size-[200px] lg:size-[300px] xl:size-[400px]' src={project1}></Image>
-              <h1 className=' w-fit  text-xl font-semibold sm:text-sm lg:text-lg xl:text-2xl'>Project One</h1>
+              <h1 className=' w-fit  text-xl font-semibold sm:text-sm lg:text-lg xl:text-2xl'>Project Two</h1>
               <div className=' w-fit h-fit flex justify-evenly gap-2 lg:gap-4'>
                 <button className=' btn btn-md sm:max-lg:btn-sm xl:btn-lg btn-outline hover:bg-black hover:text-white text-black rounded-full text-base sm:text-sm xl:text-xl'>Github</button>
                 <button className=' btn btn-md sm:max-lg:btn-sm xl:btn-lg  btn-outline hover:bg-black hover:text-white text-black rounded-full text-base sm:text-sm xl:text-xl'>Live Demo</button>
@@ -28,7 +29,7 @@ function Projects() {
             </div>
             <div className='h-fit sm:w-fit  grid gap-3 lg:gap-4 xl:gap-6 p-5 border border-black rounded-3xl overflow-hidden justify-items-center sm:max-lg:col-span-2'>
               <Image className=' rounded-3xl justify-self-center sm:size-[200px] lg:size-[300px] xl:size-[400px]' src={project1}></Image>
-              <h1 className=' w-fit  text-xl font-semibold sm:text-sm lg:text-lg xl:text-2xl'>Project One</h1>
+              <h1 className=' w-fit  text-xl font-semibold sm:text-sm lg:text-lg xl:text-2xl'>Project Three</h1>
               <div className=' w-fit h-fit flex justify-evenly gap-2 lg:gap-4'>
                 <button className=' btn btn-md sm:max-lg:btn-sm xl:btn-lg btn-outline hover:bg-black hover:text-white text-black rounded-full text-base sm:text-sm xl:text-xl'>Github</button>
                 <button className=' btn btn-md sm:max-lg:btn-sm xl:btn-lg  btn-outline hover:bg-black hover:text-white text-black rounded-full text-base sm:text-sm xl:text-xl'>Live Demo</button>
